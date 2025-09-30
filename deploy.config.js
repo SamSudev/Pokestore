848e896d55d657af80bfafc75e4450a9d316aa51
Sam <samirsierra121@gmail.com>
2025-09-30 09:57:39 -0500

    Preparar despliegue en GitHub Pages

---
+++ deploy.config.js
@@ -0,0 +1,8 @@
+// Archivo de configuración para desplegar en GitHub Pages con Vite
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/Pokestore/'
+});
